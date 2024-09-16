1b47db4d21ff0715c4211a82abadbe448f7afda2
JeanIrad <jado.milton@gmail.com>
2024-09-16 06:23:21 +0200

    adding a project feature

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -1,6 +1,11 @@
-import Skill from "./skillsWrapper/Skill";
-import reactSkill from "../../public/skills/reactjs-svgrepo-com.svg";
-import nextjsSkill from "../../public/skills/nextjs-fill-svgrepo-com.svg";
+import Skill from "@/components/skills/skillsWrapper/Skill";
+import reactjs from "@/public/skills/reactjs-svgrepo-com.svg";
+import nextjs from "@/public/skills/nextjs-fill-svgrepo-com.svg";
+import mongodb from "@/public/skills/mongo-svgrepo-com.svg";
+import nodejs from "@/public/skills/nodejs-icon-svgrepo-com.svg";
+import postgresql from "@/public/skills/postgresql-svgrepo-com.svg";
+import expressjs from "@/public/skills/express-svgrepo-com(1).svg";
+import nestjs from "@/public/skills/nestjs-svgrepo-com.svg";
 
 export default function Skills() {
   return (
@@ -8,12 +13,14 @@ export default function Skills() {
       <h1 className="font-semibold text-sky-900/80 text-3xl text-center">
         Skills
       </h1>
-      <div className="flex w-full flex-wrap sm:gap-6 sm:flex-nowrap gap-4 items-center justify-center ">
-        <Skill name="Reactjs" icon={reactSkill} />
-        <Skill name="Nextjs" icon={nextjsSkill} />
-        <Skill name="Reactjs" icon={reactSkill} />
-        <Skill name="Reactjs" icon={reactSkill} />
-        <Skill name="Reactjs" icon={reactSkill} />
+      <div className="flex w-full flex-wrap gap-6 sm:flex-nowrap  items-center justify-center ">
+        <Skill name="Reactjs" icon={reactjs} />
+        <Skill name="Nextjs" icon={nextjs} />
+        <Skill name="Nestjs" icon={nestjs} />
+        <Skill name="Expressjs" icon={expressjs} />
+        <Skill name="Postgresql" icon={postgresql} />
+        <Skill name="Mongodb" icon={mongodb} />
+        <Skill name="Nodejs" icon={nodejs} />
       </div>
     </div>
   );
